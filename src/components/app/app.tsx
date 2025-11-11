1c6c53748a194fd3afea1908e48db616df727373
aldaniil <alkhanov@t8.ru>
2025-11-11 21:21:51 +0300

    Make simple routing with placeholders

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,115 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { Route, Routes } from 'react-router-dom';
 
-import { AppHeader } from '@components';
+import {
+  AppHeader,
+  IngredientDetails,
+  Modal,
+  OrderInfo,
+  RouteProtected
+} from '@components';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+function App() {
+  const modalClose = () => {
+    console.log('modal_close');
+  };
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route
+          path='/login'
+          element={
+            <RouteProtected>
+              <Login />
+            </RouteProtected>
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <RouteProtected>
+              <Register />
+            </RouteProtected>
+          }
+        />
+        <Route
+          path='/forgot-password'
+          element={
+            <RouteProtected>
+              <ForgotPassword />
+            </RouteProtected>
+          }
+        />
+        <Route
+          path='/reset-password'
+          element={
+            <RouteProtected>
+              <ResetPassword />
+            </RouteProtected>
+          }
+        />
+        <Route
+          path='/profile'
+          element={
+            <RouteProtected>
+              <Profile />
+            </RouteProtected>
+          }
+        />
+        <Route
+          path='/profile/orders'
+          element={
+            <RouteProtected>
+              <ProfileOrders />
+            </RouteProtected>
+          }
+        />
+        <Route path='*' element={<NotFound404 />} />
+        <Route
+          path='/feed/:number'
+          element={
+            <Modal title='example' onClose={modalClose}>
+              <OrderInfo />
+            </Modal>
+          }
+        />
+        <Route
+          path='/ingredients/:id'
+          element={
+            <Modal title='example' onClose={modalClose}>
+              <IngredientDetails />
+            </Modal>
+          }
+        />
+        <Route
+          path='/profile/orders/:number'
+          element={
+            <RouteProtected>
+              <Modal title='example' onClose={modalClose}>
+                <OrderInfo />
+              </Modal>
+            </RouteProtected>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
---
+++ src/components/route-protected/route-protected.tsx
@@ -0,0 +1,8 @@
+type RouteProtectedProps = {
+  children: React.ReactElement;
+};
+
+export const RouteProtected = ({ children }: RouteProtectedProps) => {
+  const _ = 1243;
+  return children;
+};
